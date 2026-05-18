80b0edbedddc10aab869ae485f896e2ee1fda432
anujaekanayaka7-lang <anujaekanayaka7@gmail.com>
2026-05-18 19:50:18 +0530

    Update service worker caching logic and assets

--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
-const CACHE_NAME = 'lakshan-lyrics-cache-v1';
-const urlsToCache = [
+const CACHE_NAME = 'lyrics-pro-v1';
+const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+// Activate Service Worker
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker Activated');
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
   );
 });
