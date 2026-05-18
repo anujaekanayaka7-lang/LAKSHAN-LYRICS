b54f7fae764638981c7c08afb398c1159212b190
anujaekanayaka7-lang <anujaekanayaka7@gmail.com>
2026-05-18 10:50:01 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener('fetch', function(event) {
-  // App එක Phone එකට Install වීමට උදව් වන මූලික කේතය
+const CACHE_NAME = 'lakshan-lyrics-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
